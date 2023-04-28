fd67dd32b55d51b242f05b31b6515a78ab933bb7
Paula Ferreira <paulaalexandra1903@hotmail.com>
2023-04-28 10:18:32 -0300

    estilizando a tabela de empresas

--- web/src/components/ListaEmpresa/index.tsx
+++ web/src/components/ListaEmpresa/index.tsx
@@ -8,18 +8,38 @@ interface Empresa{
   cep: string,
 }
 
+type Cep = {
+  cep: string,
+  uf: string,
+  cidade: string,
+  bairro: string,
+  logradouro: string,
+}
+
 export default function ListaEmpresa(){
-  const [empresa, setEmpresas] = useState<Empresa[]>();
+  const [empresa, setEmpresas] = useState<Empresa[]>([]);
+  const [cep, setCep] = useState<Cep>({
+    cep: "",
+    uf: "",
+    cidade: "",
+    bairro: "",
+    logradouro: ""
+  });
 
-  const companyData  = [
-    {
-      nomeFantasia: "Paula's Company",
-    },
-    {
-      nomeFantasia: "Jose da Silva Company"
+  // Apagar Empresa
+  async function handleCompanyDeletion(){
+    try{
+      const response = await api.delete("/ApagarEmpresa");
+
+      setEmpresas(response.data);
+      
+    }catch(ex){
+      console.log(ex);
+      
     }
-  ]
+  }
 
+  // Listar Empresa
   async function getCompanies(){
     try{
       const response = await api.get("/listarTodasEmpresas");
@@ -32,20 +52,38 @@ export default function ListaEmpresa(){
     }
   }
 
+  const cepNumero = "65081264";
+
+  async function getCompanyAddress() {
+    const response = await fetch('http://cep.la/' + cepNumero, {
+        headers: {"Accept":"application/json"}
+    });
+    
+    const cepResponse = await response.json();
+
+    setCep(cepResponse);
+    
+  }
+
   useEffect(() => {
     getCompanies();
+    getCompanyAddress();
   }, [])
 
   return(
     <div className="w-full flex flex-col items-center">
       <Header />
-      <div>
-        <table className="table-auto bg-sky-500 box-border mt-4 p-4 rounded-md items-center">
-          <thead>
+      <div className="flex flex-col items-center">
+        <table className="table-fixed w-3/4 bg-sky-500 box-border mt-4 p-4 rounded-md items-center text-white">
+          <thead className="bg-sky-800">
             <tr>
               <th>Empresa</th>
-              <th>Edit</th>
-              <th>Delete</th>
+              <th>CNPJ</th>
+              <th>Cep</th>
+              <th>Endereço</th>
+              <th>Fornecedores</th>
+              <th>Editar</th>
+              <th>Apagar</th>
             </tr>
           </thead>
           <tbody>
@@ -55,10 +93,29 @@ export default function ListaEmpresa(){
                   {data.nomeFantasia}
                 </td>
                 <td>
-                  <button> Editar </button>
+                  {data.cnpj}
                 </td>
                 <td>
-                  <button> Apagar </button>
+                  {data.cep}
+                </td>
+                <td>
+                  {cep.logradouro + " " + cep.bairro + " - " + cep.cidade + "/" + cep.uf}
+                </td>
+                <td>
+                <button
+                    className="bg-sky-200 m-4 p-2 rounded font-bold text-sky-800"
+                  > Ver Lista </button>
+                </td>
+                <td>
+                  <button
+                    className="bg-sky-200 m-4 p-2 rounded font-bold text-sky-800"
+                  > Editar </button>
+                </td>
+                <td>
+                  <button
+                    className="bg-sky-200 m-4 p-2 rounded font-bold text-sky-800"
+                    onClick={handleCompanyDeletion}
+                  > Apagar </button>
                 </td>
               </tr>
             ))}
